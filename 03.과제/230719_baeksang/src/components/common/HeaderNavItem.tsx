337c30a3508ab639ab1392057915cc9d22c649f7
teno4944 <qqllio4944@gmail.com>
2023-07-20 08:41:28 +0900

    [FEAT] 헤더컴포넌트 네비게이션 active 액션 변경 - 글자가 아닌 NavLink 의 클래스를 active class 로 지정

--- 03.과제/230719_baeksang/src/components/common/HeaderNavItem.tsx
+++ 03.과제/230719_baeksang/src/components/common/HeaderNavItem.tsx
@@ -8,10 +8,8 @@ type Props = {
 export const HeaderNavItem = ({ path, name }: Props) => {
   return (
     <li key={path}>
-      <NavLink to={path}>
-        {({ isActive }) => (
-          <span className={isActive ? 'text-blue-500 font-bold underline' : 'text-gray-900'}>{name}</span>
-        )}
+      <NavLink to={path} className={({ isActive }) => (isActive ? 'on' : '')}>
+        <span>{name}</span>
       </NavLink>
     </li>
   );
